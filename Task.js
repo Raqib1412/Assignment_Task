e2eb62a077cbc3cf0d6dfdf4cc9b85f11519d3f2
Raqib1412 <134727904+Raqib1412@users.noreply.github.com>
2025-01-20 15:47:45 +0530

    Delete Task.js

--- Task.js
+++
@@ -1,9 +0,0 @@
-// Collapsible Left Menu
-const menuToggle = document.getElementById('menuToggle');
-const leftMenu = document.getElementById('leftMenu');
-
-menuToggle.addEventListener('click', () => {
-    leftMenu.classList.toggle('hidden');
-    const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
-    menuToggle.setAttribute('aria-expanded', !isExpanded);
-});
